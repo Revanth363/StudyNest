0851ba3f3f023398170be7f62e91223cf57d51d0
Revanth363 <scs837838revanth10c1a@gmail.com>
2026-05-30 09:52:42 +0530

    Fix production auth cookies

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -7,11 +7,11 @@ const generateToken = (res, userId) => {
 
   // Set token in HTTP-only cookie
   res.cookie("token", token, {
-    httpOnly: true,                                    // JS cannot access it
-    secure: process.env.NODE_ENV === "production",     // HTTPS only in prod
-    sameSite: "strict",                                // CSRF protection
-    maxAge: 7 * 24 * 60 * 60 * 1000,                 // 7 days in ms
-  });
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+});
 
   return token;
 };
